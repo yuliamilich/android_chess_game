3162a0e3bd266dc989ebc1d8dc05bdbb2be58d4d
milic <milich.yulia@gmail.com>
2018-11-05 11:21:32 +0200

    the data base works!! i can add users and see them!

--- app/src/main/java/com/yulia/milich/chess/ViewDB.java
+++ app/src/main/java/com/yulia/milich/chess/ViewDB.java
@@ -1,15 +1,20 @@
 package com.yulia.milich.chess;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class ViewDB extends AppCompatActivity {
+public class ViewDB extends AppCompatActivity implements View.OnClickListener{
 
     SQLiteDatabase sqdb;
     DBUsers users;
@@ -27,14 +32,20 @@ public class ViewDB extends AppCompatActivity {
         users = new DBUsers(this);
         sqdb = users.getWritableDatabase();
 
+//        users.onUpgrade(sqdb,1,2);
+
+        Button newUser = (Button) findViewById(R.id.newUser);
+        newUser.setOnClickListener(this);
+
+        Button delete = (Button) findViewById(R.id.delete);
+        delete.setOnClickListener(this);
+
         read_fromDC();
-        table_close();
+        //table_close();
     }
 
     public void read_fromDC() {
-        Cursor c = sqdb.query(
-                DBUsers.TABLE_NAME, null, null, null, null, null
-                , null);
+        Cursor c = sqdb.query(DBUsers.TABLE_NAME, null, null, null, null, null, null);
         c.moveToFirst();
         while (!c.isAfterLast()) {
             int idColIndex = c.getColumnIndex("_id");
@@ -61,5 +72,61 @@ public class ViewDB extends AppCompatActivity {
     public void table_close() {
         sqdb.close();
         users.close();
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId())
+        {
+            case R.id.delete:
+//                AlertDialog.Builder builder = new AlertDialog.Builder(ViewDB.this);
+//                builder.setCancelable(true);
+//                if(userChosen){
+//                    builder.setTitle("Delete the user you chose");
+//                    builder.setMessage("Are you sure you want to delete it?");
+//                    builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            //delete the user
+//                            dialog.cancel();
+//                        }
+//                    });
+//                    builder.setNeutralButton("No", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            dialog.cancel();
+//                        }
+//                    });
+//                }
+//                else{
+//                    builder.setTitle("Delete all the users");
+//                    builder.setMessage("Are you sure you want to delete EVERYTHING?");
+//                    builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            //delete all the data
+//                            users.onUpgrade(sqdb,0,0);
+//                            dialog.cancel();
+//                        }
+//                    });
+//                    builder.setNeutralButton("No", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            dialog.cancel();
+//                        }
+//                    });
+//                }
+//
+//                    builder.show();
+                break;
+            case R.id.newUser:
+                intent = new Intent(this, SignUp.class);
+
+//            default: finishActivity(1);
+//                finish();
+//                break;
+        }
+        startActivity(intent);
     }
 }
