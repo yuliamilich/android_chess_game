d13fe70a92b595509d39afc35cc880200fc937fa
milic <milich.yulia@gmail.com>
2019-02-10 20:23:45 +0200

    the menu and the battery work

--- app/src/main/java/com/yulia/milich/chess/MainMenu.java
+++ app/src/main/java/com/yulia/milich/chess/MainMenu.java
@@ -3,6 +3,9 @@ package com.yulia.milich.chess;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -20,8 +23,8 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener 
         Button help = (Button) findViewById(R.id.logIn);
         help.setOnClickListener(this);
 
-        Button exit = (Button) findViewById(R.id.exit);
-        help.setOnClickListener(this);
+        Button battery = (Button) findViewById(R.id.battery);
+        battery.setOnClickListener(this);
     }
 
     public void onClick(View v) {
@@ -34,8 +37,8 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener 
             case R.id.logIn:
                 intent = new Intent(this, LogIn.class);
                 break;
-            case R.id.exit:
-                finish();
+            case R.id.battery:
+                intent = new Intent(this, BatteryLevel.class);
                 break;
 
             default: finishActivity(1);
@@ -43,5 +46,33 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener 
                 break;
         }
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_all, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.btnMusic:
+                // User chose the "btnMusic" item, show the app settings UI...
+                return true;
+
+            case R.id.btnMute:
+                // User chose the "btnMute" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
--- app/src/main/java/com/yulia/milich/chess/TheChessGame.java
+++ app/src/main/java/com/yulia/milich/chess/TheChessGame.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
 import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AbsListView;
@@ -266,6 +268,14 @@ public class TheChessGame extends AppCompatActivity implements View.OnClickListe
 
         builder.show();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_all, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
